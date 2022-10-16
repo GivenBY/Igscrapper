ab244535d798b5b1b9027ebf892093a0bcaa49b8
Suryakant_2003 <32999024+GivenBY@users.noreply.github.com>
2022-10-16 17:02:21 +0530

    Add files via upload

---
+++ Igscrapper.py
@@ -0,0 +1,20 @@
+import json
+import requests
+import os
+import os, sys
+sys.path.append(os.getcwd()+"/.lib/")
+import argparse
+ap = argparse.ArgumentParser()
+ap.add_argument("-u", "--user", required=True, help="Enter User to Scan")
+args = vars(ap.parse_args())
+os.system("clear")
+if args['user']:
+	username=args["user"]
+	url = f"https://instagram188.p.rapidapi.com/userid/{username}"
+	headers = {
+	"X-RapidAPI-Key": "529183e6d5msh15e3e2c37b1dc5ep154286jsnaba59a2fe040",
+	"X-RapidAPI-Host": "instagram188.p.rapidapi.com"}
+	response = requests.request("GET", url,headers=headers)
+	a=response.json()
+	if a['success']==True:
+		userid=a['data']
