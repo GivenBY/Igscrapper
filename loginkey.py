a6bfd5a1cd9d3cca5a7d119c59b27a99b8e3b15b
Suryakant_2003 <32999024+GivenBY@users.noreply.github.com>
2022-10-16 22:19:21 +0530

    Delete loginkey.py

--- loginkey.py
+++
@@ -1,8 +0,0 @@
-import os
-import argparse
-ap = argparse.ArgumentParser()
-ap.add_argument("-k", "--key", required=True, help="key")
-args = vars(ap.parse_args())
-os.system("clear")
-if args['key']:
-	loginkey=args["key"]
