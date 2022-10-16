2f70d433f40bc5d47f735fd47b063b6144ba39c6
Suryakant_2003 <32999024+GivenBY@users.noreply.github.com>
2022-10-16 22:51:57 +0530

    Update Igscrapper.py

--- Igscrapper.py
+++ Igscrapper.py
@@ -5,13 +5,12 @@ ap = argparse.ArgumentParser()
 ap.add_argument("-u", "--user", required=True, help="Enter User to Scan")
 args = vars(ap.parse_args())
 os.system("clear")
+loginkey=input("Enter Key Generated From Rapid Api: ")
 if args['user']:
 	username=args["user"]
 	print("Connectiong to Server:")
 	url = f"https://instagram188.p.rapidapi.com/userid/{username}"
-	headers = {
-	"X-RapidAPI-Key": "529183e6d5msh15e3e2c37b1dc5ep154286jsnaba59a2fe040",
-	"X-RapidAPI-Host": "instagram188.p.rapidapi.com"}
+	headers = {"X-RapidAPI-Key": f"{loginkey}","X-RapidAPI-Host": "instagram188.p.rapidapi.com"}
 	response = requests.request("GET", url,headers=headers)
 	a=response.json()
 	if a['success']==True:
@@ -19,10 +18,26 @@ if args['user']:
 		userid=a['data']
 	else:
 		print("Connection Failed")
-print("Enter Commmands")
+
+# To Download Profile picture
 def profile_picture(username):
-	url =f'https://instagram188.p.rapidapi.com/userphoto/{username}'
+	url =f"https://instagram188.p.rapidapi.com/userphoto/{username}"
 	response = requests.request("GET", url,headers=headers)
 	a=response.json()
 	urllib.request.urlretrieve(a['data'],r'Output/profile_image.jpg')
 	print("Image Saved in Output Folder")
+
+
+print("Enter Commmands\nContact info : i\nProfile Photo : p\nQuit : q")
+loop=True
+while loop :
+	command=input(">>")
+	if command=="p":
+		profile_picture(username)
+	elif command=="i":
+		pass
+	elif command=="q":
+		loop=False
+	else:
+		print("Invalid Command")
+
