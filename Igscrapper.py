d97e8f525669213e523251e0d90ed935a09c4b2a
Suryakant_2003 <32999024+GivenBY@users.noreply.github.com>
2022-10-16 20:11:24 +0530

    Update Igscrapper.py

--- Igscrapper.py
+++ Igscrapper.py
@@ -1,11 +1,13 @@
 import requests,os
 import argparse
+import urllib.request
 ap = argparse.ArgumentParser()
 ap.add_argument("-u", "--user", required=True, help="Enter User to Scan")
 args = vars(ap.parse_args())
 os.system("clear")
 if args['user']:
 	username=args["user"]
+	print("Connectiong to Server:")
 	url = f"https://instagram188.p.rapidapi.com/userid/{username}"
 	headers = {
 	"X-RapidAPI-Key": "529183e6d5msh15e3e2c37b1dc5ep154286jsnaba59a2fe040",
@@ -13,4 +15,14 @@ if args['user']:
 	response = requests.request("GET", url,headers=headers)
 	a=response.json()
 	if a['success']==True:
+		print("Connection Successful")
 		userid=a['data']
+	else:
+		print("Connection Failed")
+print("Enter Commmands")
+def profile_picture(username):
+	url =f'https://instagram188.p.rapidapi.com/userphoto/{username}'
+	response = requests.request("GET", url,headers=headers)
+	a=response.json()
+	urllib.request.urlretrieve(a['data'],r'Output/profile_image.jpg')
+	print("Image Saved in Output Folder")
