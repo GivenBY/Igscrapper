0914eb6557e5a63fa0a9cc64f1d4be7f2ba8b863
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2022-10-16 23:19:13 +0530

    Update igscrapper.sh

--- igscrapper.sh
+++ igscrapper.sh
@@ -4,7 +4,7 @@
 function USERPHOTO() {
 C=$(curl --request GET --url https://instagram188.p.rapidapi.com/userphoto/$1 --header 'X-RapidAPI-Host: instagram188.p.rapidapi.com' --header 'X-RapidAPI-Key: Your-key')
 D=$(echo "$C" | awk '{ print substr ($0, 25, length($0)-3 ) }' | rev | cut -c3- | rev |  sed 's/[\]//g' )
-curl "$D" -o Output/img/1.jpg
+curl "$D" -o Output/1.jpg
 }
 
 USERPHOTO "$1"
