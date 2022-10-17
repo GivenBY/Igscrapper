34eb278c6d61a111cd3e4bc3ac2d707db97b6320
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2022-10-17 22:55:55 +0530

    Update igscrapper.sh

--- igscrapper.sh
+++ igscrapper.sh
@@ -1,10 +1,16 @@
+
 #!/bin/bash
 
+KEY='ENTER YOUR API KEY'
 
-function USERPHOTO() {
-C=$(curl --request GET --url https://instagram188.p.rapidapi.com/userphoto/$1 --header 'X-RapidAPI-Host: instagram188.p.rapidapi.com' --header 'X-RapidAPI-Key: Your-key')
-D=$(echo "$C" | awk '{ print substr ($0, 25, length($0)-3 ) }' | rev | cut -c3- | rev |  sed 's/[\]//g' )
-curl "$D" -o Output/1.jpg
-}
 
-USERPHOTO "$1"
+#UserID
+A=$(curl --request GET 	--url https://instagram188.p.rapidapi.com/userid/$1 --header 'X-RapidAPI-Host: instagram188.p.rapidapi.com' --header "X-RapidAPI-Key: $KEY")
+B=$(echo "$A" | awk '{ print substr ($0, 25 ) }' | rev | cut -c3- | rev  )
+echo "UserID is : $B"
+echo
+#UserContactDetails
+E=$(curl --request GET --url https://instagram188.p.rapidapi.com/usercontact/$B --header 'X-RapidAPI-Host: instagram188.p.rapidapi.com' --header "X-RapidAPI-Key: $KEY" )
+USERNAME=$1
+echo "$E" >  Output/$USERNAME_1.json
+cat Output/$USERNAME_1.json | jq | tee Output/$USERNAME.json
