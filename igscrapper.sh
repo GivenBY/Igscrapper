dba35ce1762517a480de139f895979e4c03cc9ba
Harsh8266 <58392509+harsh8266@users.noreply.github.com>
2022-10-20 00:43:42 +0530

    Banner

--- igscrapper.sh
+++ igscrapper.sh
@@ -3,7 +3,22 @@
 
 KEY='ENTER YOUR API KEY'
 command -v jq > /dev/null 2>&1 || { echo >&2 "I require jq but it's not installed. Install it. Aborting. ";echo "sudo apt install jq"; exit 1; }
+echo "###
+  #   ####   ####   ####  #####    ##   #####  #####  ###### #####
+  #  #    # #      #    # #    #  #  #  #    # #    # #      #    #
+  #  #       ####  #      #    # #    # #    # #    # #####  #    #
+  #  #  ###      # #      #####  ###### #####  #####  #      #####
+  #  #    # #    # #    # #   #  #    # #      #      #      #   #
+ ###  ####   ####   ####  #    # #    # #      #      ###### #    #
+"
 
+if [ -z $1 ]
+then
+        echo "enter you user"
+        echo " how to use:-
+                ./igscrapper.sh username"
+        exit
+fi
 #UserID
 A=$(curl --request GET 	--url https://instagram188.p.rapidapi.com/userid/$1 --header 'X-RapidAPI-Host: instagram188.p.rapidapi.com' --header "X-RapidAPI-Key: $KEY")
 B=$(echo "$A" | awk '{ print substr ($0, 25 ) }' | rev | cut -c3- | rev  )
